927d216cb1e483a60762c3024d86af0913b6bc4b
Mhirley Lopes <mhiloca@gmail.com>
2019-09-14 19:06:14 -0300

    created app attractions
    registered Attractio model in admin site
    created a M2M pontoturistico and attraction

--- core/models.py
+++ core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from attractions.models import Attraction
 
 
 class PontoTuristico(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
-    work_hours = models.TextField()
-    min_age = models.IntegerField()
+    status = models.BooleanField(default=True)
+    attraction_list = models.ManyToManyField(Attraction)
 
     def __str__(self):
         return self.name
