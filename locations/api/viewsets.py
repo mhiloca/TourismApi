58d345fe8411f2e989aeb5611e227d642817d8f5
Mhirley Lopes <mhiloca@gmail.com>
2019-09-15 17:34:14 -0300

    implemented permission_class in LocationViewSet

--- locations/api/viewsets.py
+++ locations/api/viewsets.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from locations.models import Location
 from .serializers import LocationSerializer
 
@@ -7,3 +10,6 @@ class LocationViewSet(viewsets.ModelViewSet):
 
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    authentication_classes = (TokenAuthentication,)
+
