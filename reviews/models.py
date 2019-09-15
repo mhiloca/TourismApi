04c16af6e1a73bd882f7015d4b4809de3eeb9d0d
Mhirley Lopes <mhiloca@gmail.com>
2019-09-14 22:19:34 -0300

    chaged Review.grade max_digits=4

--- reviews/models.py
+++ reviews/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     saying = models.TextField()
-    grade = models.DecimalField(max_digits=3, decimal_places=2)
+    grade = models.DecimalField(max_digits=4, decimal_places=2)
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
